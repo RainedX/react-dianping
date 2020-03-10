8faba41ff49265fcefa39c59372cbc6c9166b7a6
RainedX <1355247497@qq.com>
2020-03-11 07:57:12 +0800

    change

--- src/redux/modules/home.js
+++ src/redux/modules/home.js
@@ -1,6 +1,14 @@
 import url from '../../utils/url';
-import { FETCH_DATA } from "../middleware/api";
-import { schema } from "./entities/products";
+import { FETCH_DATA } from '../middleware/api';
+import { schema } from './entities/products';
+import { combineReducers } from 'redux';
+
+const params = {
+  PATH_LIKES: 'likes',
+  PATH_DISCOUNTS: 'discounts',
+  PAGE_SIZE_LIKES: 5,
+  PAGE_SIZE_DISCOUNT: 3
+};
 
 export const types = {
   // 获取猜你喜欢请求
@@ -8,7 +16,13 @@ export const types = {
   // 请求成功
   FETCH_LIKES_SUCCESS: 'HOME/FETCH_LIKES_SUCCESS',
   // 请求失败
-  FETCH_LIKES_FAILURE: 'HOME/FETCH_LIKES_FAILURE'
+  FETCH_LIKES_FAILURE: 'HOME/FETCH_LIKES_FAILURE',
+  // 获取猜你喜欢请求
+  FETCH_DISCOUNTS_REQUEST: 'HOME/FETCH_DISCOUNTS_REQUEST',
+  // 猜你喜欢请求成功
+  FETCH_DISCOUNTS_SUCCESS: 'HOME/FETCH_DISCOUNTS_SUCCESS',
+  // 猜你喜欢请求失败
+  FETCH_DISCOUNTS_FAILURE: 'HOME/FETCH_DISCOUNTS_FAILURE'
 };
 
 const fetchLikes = (endpoint, params) => ({
@@ -19,34 +33,95 @@ const fetchLikes = (endpoint, params) => ({
       types.FETCH_LIKES_FAILURE
     ],
     endpoint,
-    schema,
-    params
+    schema
+  },
+  ...params
+});
+
+const fetchDiscounts = endpoint => ({
+  [FETCH_DATA]: {
+    types: [
+      types.FETCH_DISCOUNTS_REQUEST,
+      types.FETCH_DISCOUNTS_SUCCESS,
+      types.FETCH_DISCOUNTS_FAILURE
+    ],
+    endpoint,
+    schema
   }
-})
+});
+
+const initialState = {
+  likes: {
+    isFetching: false,
+    pageCount: 0,
+    ids: []
+  },
+  discounts: {
+    isisFetching: false,
+    ids: []
+  }
+};
 
 export const actions = {
   loadLikes: () => {
     return (dispatch, getState) => {
-      const endpoint = url.getProductList(0, 10)
-      return dispatch(fetchLikes(endpoint))
-    }
+      const { pageCount } = getState().home;
+      const rowIndex = pageCount * params.PAGE_SIZE_LIKES;
+      const endpoint = url.getProductList(
+        params.PATH_LIKES,
+        rowIndex,
+        params.PAGE_SIZE_LIKES
+      );
+      dispatch(fetchLikes(endpoint));
+    };
+  },
+  loadDiscounts: () => {
+    return (dispatch, getState) => {
+      const endpoint = url.getProductList(
+        params.PATH_DISCOUNTS,
+        0,
+        params.PAGE_SIZE_DISCOUNT
+      );
+      dispatch(fetchDiscounts(endpoint));
+    };
   }
 };
 
-const reducer = (state = {}, action) => {
+const likesReducer = (state = initialState.likes, action) => {
   switch (action.type) {
     case types.FETCH_LIKES_REQUEST:
-      // todo
-      return state;
+      return { ...state, isFetching: true };
     case types.FETCH_LIKES_SUCCESS:
-      // todo
-      return state;
+      return {
+        ...state,
+        pageCount: state.pageCount + 1,
+        isFetching: false,
+        ids: state.ids.concat(action.response.ids)
+      };
     case types.FETCH_LIKES_FAILURE:
-      // todo
-      return state;
+      return { ...state, isFetching: false };
     default:
       return state;
   }
 };
+
+const discountsReducer = (state = initialState.discounts, action) => {
+  switch (action.type) {
+    case types.FETCH_DISCOUNTS_REQUEST:
+      return { ...state, isFetching: true };
+    case types.FETCH_DISCOUNTS_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        ids: state.ids.concat(action.response.ids)
+      };
+    case types.FETCH_DISCOUNTS_FAILURE:
+      return { ...state, isFetching: false };
+    default:
+      return state;
+  }
+};
+
+const reducer = combineReducers({ likesReducer, discountsReducer });
 
 export default reducer;
