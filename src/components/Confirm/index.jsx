cb0ef10e36d31117f9a1ceee82df087c591a0d50
RainedX <1355247497@qq.com>
2020-03-30 16:26:54 +0800

    change

--- src/components/Confirm/index.jsx
+++ src/components/Confirm/index.jsx
@@ -1,11 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import './index.css'
 
 class Confirm extends Component {
   render() {
+    const {
+      content,
+      onCancel,
+      cancelText,
+      onConfirm,
+      confirmText
+    } = this.props;
     return (
-      <div>Confirm</div>
-    )
+      <div className="confirm">
+        <div className="confirm__alert">
+          <div className="confirm__content">{content}</div>
+          <div className="confirm__btns">
+            <Link to='#' className="confirm__btn" onClick={onCancel}>
+              {cancelText}
+            </Link>
+            <Link to='#' className="confirm__btn" onClick={onConfirm}>
+              {confirmText}
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/containers/User/index.jsx
+++ src/containers/User/index.jsx
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import UserMain from './containers/UserMain';
+import UserHeader from './components/UserHeader';
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actions as loginActions } from "../../redux/modules/login";
 
 class User extends Component {
+  handleBack = () => {
+    this.props.history.push("/")
+  }
+
+  handleLogout = () => {
+    this.props.loginActions.logout();
+  };
+
   render() {
-    return <div>user</div>;
+    return (
+      <div>
+        <UserHeader
+          onBack={this.handleBack}
+          onLogout={this.handleLogout}
+        />
+        <UserMain />
+      </div>
+    );
   }
 }
 
-export default User;
+const mapStateToProps = (state, props) => {
+  return {}
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    loginActions: bindActionCreators(loginActions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(User);
